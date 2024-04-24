a1697e0fd193e20e2ca525facbbf64846b176bc2
Matthew Pieterse <63480247+mattpieterse@users.noreply.github.com>
2024-04-24 18:30:53 +0200

    Add method to update data object collection

--- WordCounterWPF/ViewModels/HomeViewModel.cs
+++ WordCounterWPF/ViewModels/HomeViewModel.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections.ObjectModel;
 using WordCounterWPF.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WordCounterWPF.ViewModels;
 
 public class HomeViewModel {
 
-    public ObservableCollection<TextDataObject> DataObjects { get; }
+    public ObservableCollection<TextDataObject> DataObjects { get; set; }
 
     public HomeViewModel() {
+        DataObjects = TextDataController.GetDataObjects();
+    }
+
+    public void UpdateObjectDatabase(string text) {
+        TextDataController.Clear();
+        ObservableCollection<TextDataObject> StagingDB = [
+            new TextDataObject("Length", TextInterpreter.GetTotalLength(text)),
+            new TextDataObject("Word Count", TextInterpreter.GetWordCount(text)),
+            new TextDataObject("Character Count", TextInterpreter.GetCharCount(text)),
+        ];
+
+        foreach (TextDataObject DataObject in StagingDB) {
+            TextDataController.AddDataObject(DataObject);
+        }
+
         DataObjects = TextDataController.GetDataObjects();
     }
 }
