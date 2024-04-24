5254bb50bd87fbe18d68f2c40bd3ddf48f6b9d3c
Matthew Pieterse <63480247+mattpieterse@users.noreply.github.com>
2024-04-24 18:14:19 +0200

    Add methods to return counts of statistics

--- WordCounterWPF/Models/TextInterpreter.cs
+++ WordCounterWPF/Models/TextInterpreter.cs
@@ -1,5 +1,23 @@
 ﻿namespace WordCounterWPF.Models;
 
 public class TextInterpreter {
-    // TODO: ...
+
+    public static int GetTotalLength(string text) {
+        return text.Length;
+    }
+
+    public static int GetWordCount(string text) {
+        string trimmedText = text.Trim();
+        return trimmedText.Length == 0 ? 0 : trimmedText.Split(' ', '\t', '\n').Length;
+    }
+
+    public static int GetCharCount(string text) {
+        int count = 0;
+        foreach (char c in text) {
+            if (!char.IsWhiteSpace(c)) count++;
+        }
+        return count;
+    }
+
+    // TODO: Create additional parse methods
 }
