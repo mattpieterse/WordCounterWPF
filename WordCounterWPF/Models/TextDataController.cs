66830a9ead658067ae650155f44fc947d1d05b7c
Matthew Pieterse <63480247+mattpieterse@users.noreply.github.com>
2024-04-24 16:59:53 +0200

    Add get/set and clear methods to interact with the collection

--- WordCounterWPF/Models/TextDataController.cs
+++ WordCounterWPF/Models/TextDataController.cs
@@ -6,7 +6,26 @@ public class TextDataController {
 
     private static ObservableCollection<TextDataObject> _Database = new ObservableCollection<TextDataObject>();
 
-    // --- Static Methods
+    // -- Static Methods
 
-    // TODO: ...
+    /// <summary>
+    /// Gets private collection of data objects.
+    /// </summary>
+    public static ObservableCollection<TextDataObject> GetDataObjects() {
+        return _Database;
+    } 
+
+    /// <summary>
+    /// Adds a new data object to the private collection.
+    /// </summary>
+    public static void AddDataObject(TextDataObject textDataObject) {
+        _Database.Add(textDataObject);
+    }
+
+    /// <summary>
+    /// Clear all objects within the collection.
+    /// </summary>
+    public static void Clear() {
+        _Database.Clear();
+    }
 }
