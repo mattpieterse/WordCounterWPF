072aca906c170fc055dc63b89e384a0b61b10fd3
Matthew Pieterse <63480247+mattpieterse@users.noreply.github.com>
2024-04-24 16:48:59 +0200

    Add private database variable to controller class for storing data objects

--- WordCounterWPF/Models/TextDataController.cs
+++ WordCounterWPF/Models/TextDataController.cs
@@ -1,4 +1,12 @@
-﻿namespace WordCounterWPF.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace WordCounterWPF.Models;
 
 public class TextDataController {
+
+    private static ObservableCollection<TextDataObject> _Database = new ObservableCollection<TextDataObject>();
+
+    // --- Static Methods
+
+    // TODO: ...
 }
